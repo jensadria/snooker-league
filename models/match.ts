c7745a0fc2693e99d538e4cd85a699330dff29b8
Jens Adria <jensadria@gmail.com>
2022-08-01 14:48:21 +0800

    Added types to allow build

--- models/match.ts
+++ models/match.ts
@@ -1,15 +1,15 @@
 export interface MatchModel {
-	match_id: number;
-	year: number;
-	match_round: number;
-	match_week: number;
+	match_id: number | null;
+	year: number | null;
+	match_round: number | null;
+	match_week: number | null;
 	date: string;
 	home_team: string;
 	away_team: string;
 	address: string;
 	location: string;
-	home_score?: number;
-	away_score?: number
+	home_score?: number | null;
+	away_score?: number | null
 }
 
 export const emptyMatch = {
