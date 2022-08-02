9b85b843841aa2f251538ce91471ba25ff91485f
Jens Adria <jensadria@gmail.com>
2022-08-02 15:00:23 +0800

    Removed login file for deployment

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import CredentialProvider from "next-auth/providers/credentials";
+//import { compare } from 'bcrypt'; 
 
 export default NextAuth({
 	providers: [
@@ -14,6 +15,24 @@ export default NextAuth({
 				password: { label: "Password", type: "password" },
 			},
 			authorize: (credentials) => {
+				//if (req.method === "POST") {
+				//	const { email, password } = req.body
+
+				//	const sql = "SELECT players.id AS player_id, players.name,email,password_hash,previous_handicap,t.name AS team_name, team_id  FROM players LEFT JOIN teams as t ON t.id = team_id WHERE email = $1"
+				//	const dbResult = await db.query(sql, [email])
+				//	const player = dbResult.rows[0]
+
+				//	compare(password, player.password_hash, (err, result) => {
+				//		if (!err && result) {
+				//			const data = { player: player }
+				//			const jwt = sign(data, process.env.SECRET_KEY, { expiresIn: '3h' })
+
+				//			res.status(200).json({ authToken: jwt })
+				//		} else {
+				//			res.json({ message: "login failed" })
+				//		}
+				//	});
+				//}
 				// database look up
 				if (
 					credentials?.username === "john" &&
