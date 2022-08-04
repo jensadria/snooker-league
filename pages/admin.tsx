b4f73ae623e11ad8ce7a62b1822c8145018a5519
Jens Adria <jensadria@gmail.com>
2022-08-04 11:21:48 +0800

    Added new default font, changed admin page to work on build

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
-import Link from "next/link";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 //interface LeagueTableProps {
 //  children: React.ReactNode;
@@ -9,19 +9,24 @@ import { useSession } from "next-auth/react";
 const AdminPage: NextPage = () => {
   const { data: session, status } = useSession();
 
-  if (!session) {
-    return (
-      <div>
+  return (
+    <div>
+      {session && (
         <Link href='/api/auth/signin'>
           <a>You need to login to proceed</a>
         </Link>
-      </div>
-    );
-  }
-
-  if (session) {
-    return <div>Test</div>;
-  }
+      )}
+      {!session && (
+        <div>
+          <p>You are logged in</p>
+          <br />
+          <Link href='/api/auth/signin'>
+            <a>Click Here To Log Out</a>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default AdminPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["IBM Plex Sans", "sans-serif"],
+      display: ["IBM Plex Sans", "sans-serif"],
+      body: ["IBM Plex Sans", "sans-serif"],
+    },
   },
   plugins: [],
 };
